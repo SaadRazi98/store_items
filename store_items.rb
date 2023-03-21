5dcea64f9d509042f3a819172af8f085cda61d08
SAAD <SAADRAZI1998@GMAIL.com>
2023-03-21 15:20:26 -0500

    some descriptive message explaining the changes

---
+++ store_items.rb
@@ -0,0 +1,22 @@
+item1 = {
+  "color" => "red", 
+  "price" => 15, 
+  "type" => "food"
+}
+
+item2 = {
+  "color" => "gray", 
+  "price" => 67, 
+  "type" => "electronics"
+}
+
+item3 = {
+  "color" => "brown", 
+  "price" => 16, 
+  "type" => "clothes"
+}
+
+
+p item1
+
+p "#{item1["color"]} apples will cost #{item1["price"]} and it will be in the #{item1["type"]} section"
